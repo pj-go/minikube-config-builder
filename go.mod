36fe8f390c98d3d6b8c730fb62039e0def25aaae
pengjian <pengjian.cool@bytedance.com>
2023-07-28 15:32:03 +0800

    init

---
+++ go.mod
@@ -0,0 +1,22 @@
+module minikube-config-builder
+
+go 1.20
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	k8s.io/client-go v0.27.4
+	sigs.k8s.io/yaml v1.3.0
+)
+
+require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.27.4 // indirect
+	k8s.io/klog/v2 v2.90.1 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	v1 "k8s.io/client-go/tools/clientcmd/api/v1"
+	"k8s.io/client-go/util/homedir"
+	"os"
+	"path/filepath"
+	"sigs.k8s.io/yaml"
+)
+
+var (
+	kubeconfig            string
+	apiServer             string
+	DefaultKubeconfigPath = filepath.Join(homedir.HomeDir(), ".kube", "config")
+)
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Paths to a kubeconfig.")
+	flag.StringVar(&apiServer, "apiServer", "", "format: https://ip:port")
+}
+
+func main() {
+	flag.Parse()
+	if len(kubeconfig) == 0 {
+		kubeconfig = DefaultKubeconfigPath
+	}
+	bs, err := os.ReadFile(kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	config := &v1.Config{}
+	err = yaml.Unmarshal(bs, config)
+	if err != nil {
+		panic(err)
+	}
+
+	spew.Dump(config)
+	fmt.Println("------")
+
+	for i, cluster := range config.Clusters {
+		if cluster.Name == "minikube" {
+			cluster.Cluster.CertificateAuthority = ""
+			cluster.Cluster.InsecureSkipTLSVerify = true
+			if len(apiServer) > 0 {
+				cluster.Cluster.Server = apiServer
+			}
+		}
+		config.Clusters[i] = cluster
+	}
+
+	for i, info := range config.AuthInfos {
+		if info.Name == "minikube" {
+			if len(info.AuthInfo.ClientCertificate) > 0 {
+				s, err := EncodeDataFromFile(info.AuthInfo.ClientCertificate)
+				if err != nil {
+					panic(s)
+				}
+				info.AuthInfo.ClientCertificateData = []byte(s)
+				info.AuthInfo.ClientCertificate = ""
+			}
+			if len(info.AuthInfo.ClientKey) > 0 {
+				s, err := EncodeDataFromFile(info.AuthInfo.ClientKey)
+				if err != nil {
+					panic(s)
+				}
+				info.AuthInfo.ClientKeyData = []byte(s)
+				info.AuthInfo.ClientKey = ""
+			}
+		}
+		config.AuthInfos[i] = info
+	}
+
+	rbs, err := yaml.Marshal(config)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("result: ")
+	fmt.Println(string(rbs))
+}
+
+func EncodeDataFromFile(p string) (string, error) {
+	bs, err := os.ReadFile(p)
+	if err != nil {
+		return "", err
+	}
+	s := base64.StdEncoding.EncodeToString(bs)
+	return s, err
+}
